5648b6fdc55bda6793c9b538e2f0e60c495c84ab
holer <2721584867@qq.com>
2025-02-07 20:47:27 +0800

    88 easy

--- src/leetcode/editor/cn/[88]合并两个有序数组.java
+++ src/leetcode/editor/cn/[88]合并两个有序数组.java
@@ -65,9 +65,9 @@ class P88_MergeSortedArray{
 	 	 //测试代码
 	 	 Solution solution = new P88_MergeSortedArray().new Solution();
 //	     int[] nums1 = {1,2,7,9,10,0,0};
-	     int[] nums1 = {1,0,0};
-	     int[] nums2 = {2,5};
-	     int m = 1;
+	     int[] nums1 = {0,0,0};
+	     int[] nums2 = {2,5,6};
+	     int m = 0;
 	     int n = nums2.length;
 	     solution.merge(nums1, m, nums2, n);
 		 for (int i = 0; i < nums1.length; i++) {
@@ -80,17 +80,18 @@ class P88_MergeSortedArray{
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
     	if (n == 0) return;
-    	int i = nums1.length - 1;
-		for (; i >= 0 && n > 0 && m > 0; i--) {
-			if (nums1[m - 1] > nums2[n - 1]) {
-				nums1[i] = nums1[--m];
+		int i = nums1.length - 1;
+		n--;m--;
+		while (i >= 0 && n >= 0 && m >= 0) {
+			if (nums2[n] > nums1[m]) {
+				nums1[i--] = nums2[n--];
 			} else {
-				nums1[i] = nums2[--n];
+				nums1[i--] = nums1[m--];
 			}
 		}
-		while (n > 0)
-			nums1[i--] = nums2[--n];
-
+		while (n >= 0) {
+			nums1[i--] = nums2[n--];
+		}
     }
     /* public void merge(int[] nums1, int m, int[] nums2, int n) {
     	if (n == 0) return;
