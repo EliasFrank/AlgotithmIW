7f29645f3eb57a5b98846d4dc0fbb8935a3ab39e
holer <2721584867@qq.com>
2025-02-17 23:33:08 +0800

    55 mid

--- src/leetcode/editor/cn/[122]买卖股票的最佳时机 II.java
+++ src/leetcode/editor/cn/[122]买卖股票的最佳时机 II.java
@@ -55,14 +55,14 @@ class BestTimeToBuyAndSellStockIi{
 class Solution {
 
 
-        /*public int maxProfit(int[] prices) {
+        public int maxProfit(int[] prices) {
         int ans = 0;
         int n = prices.length;
         for (int i = 1; i < n; i++) {
             ans += Math.max(0, prices[i] - prices[i -1]);
         }
         return ans;
-    }*/
+    }
 
     /*public int maxProfit(int[] prices) {
         int tempMin = prices[0];
